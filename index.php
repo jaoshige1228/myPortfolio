92fd02ab60a7a3171c430c0e92893e34388ec770
jaoshige1228 <jaoshige-king@au.com>
2020-07-08 15:14:42 +0900

    学習中の技術を修正

--- index.php
+++ index.php
@@ -31,12 +31,12 @@
   <section class="skillSet">
     <h1>〜学習中の技術〜</h1>
     <div>
-      <p>○ HTML\CSS　……　8ヶ月</p>
-      <p>○ JavaScript(jQuery)　……　8ヶ月</p>
-      <p>○ PHP　……　8ヶ月</p>
-      <p>○ MySQL　……　8ヶ月</p>
-      <p>○ Git　……　触れたばかり</p>
-      <p>○ Laravel　……　触れたばかり</p>
+      <p><span class="lang">○ HTML\CSS</span>　……フレックスボックスを用いて、Webサイトの基本的なマークアップが可能です。異なるデバイス間でのレスポンシブデザインも作成できます。</p>
+      <p><span class="lang">○ JavaScript(jQuery)</span>　……主にjQueryを使用し、モーダルウィンドウやアコーディオンといった動的なデザインを作成できます。また、Ajaxによる非同期通信も実装可能です。</p>
+      <p><span class="lang">○ PHP</span>　……バックエンドエンジニアを志しているため、現在最も学習に注力している言語です。自分が実装したいと思った処理は、試行錯誤しつつではありますが、作成可能です。</p>
+      <p><span class="lang">○ MySQL</span>　……PHPと連携し、データの挿入や変更、取り出しといった、基本的な処理を実行可能です。</p>
+      <p><span class="lang">○ Git</span>　……GitとGitHubを用いて、コミットやプッシュ、プル、ブランチの作成や切り替え、マージ等、基本的なバージョン管理を行えます。</p>
+      <p><span class="lang">○ Laravel</span>　……動画教材のレッスンにて、インストールから極簡易的なWebアプリ作成を行いました。次回に作成を構想しているWebアプリは、Laravelを使用して作成する予定です。</p>
     </div>
   </section>
 
